c28d35396d3c7c4b97aed7b31dee0580f6fc1efa
michelle <omekemichelle@gmail.com>
2023-03-21 18:43:17 -0700

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef main_h
+#define main_h
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,6 +11,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
