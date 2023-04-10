61ca02fe4c70a9994a7b54636ac49409f8352ddf
michelle <omekemichelle@gmail.com>
2023-04-10 03:00:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[]) {
+    // Print the first argument, which is the name of the program
+    printf("%s\n", argv[0]);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x07. C - Even more pointers, arrays and strings
+0x0A. C - argc, argv Project
