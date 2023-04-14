0cfc4b2dfa9c3e029c2a14ee2743e689b648cad3
michelle <omekemichelle@gmail.com>
2023-04-13 20:22:28 -0700

    multiple files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,23 +1,53 @@
 #include <stdlib.h>
 #include "main.h"
 
-int* allocateArray(int size) {
-    int* arr = (int*)malloc(size * sizeof(int));
-    if (arr == NULL) { 
-        printf("Memory allocation failed.\n");
-        exit(1); 
-    }
-    return arr; 
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
 
-int main() {
-    int size = 5;
-    int* arr = allocateArray(size);
-    printf("Memory allocated successfully.\n");
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
 
 
-    free(arr);
-    printf("Memory deallocated successfully.\n");
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
 
-    return 0;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
+
