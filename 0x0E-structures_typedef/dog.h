6aa703d47a65ede031bda547b53332bc31ce8ee6
michelle <omekemichelle@gmail.com>
2023-04-17 17:45:01 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,7 @@
  * Description: Longer description
  */
 
-struct dog;
+struct dog
 {
 	char *name;
 	float age;
@@ -32,5 +32,5 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-#endif /*DOG_H */
+#endif
 
