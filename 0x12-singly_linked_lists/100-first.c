9bd4ec7719423279f2b34dbe8b9e23aceffe9507
michelle <omekemichelle@gmail.com>
2023-04-28 18:50:29 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+* myStartupFun - implementation of myStartupFun.
+*/
+
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
+/**
+* main - function code
+* Return: always 0 when successful
+*/
+
+
+int main(void)
+{
+
+return (0);
+}
+
